d223406967f5eea3e1c219db5a8c7d64b40071c3
Jeremy Kirshbaum <jjkirshbaum@gmail.com>
2024-04-06 22:14:17 -0700

    font change

--- pages/_document.js
+++ pages/_document.js
@@ -1,22 +1,23 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
-  render() {
-    return (
-      <Html lang="en">
-        <Head>
-          <link
-            href="https://fonts.googleapis.com/css2?family=Segoe+UI:wght@400;700&display=swap"
-            rel="stylesheet"
-          />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+
+render() {
+
+return (
+<Html lang="en"> <Head> <link
+
+href="https://fonts.googleapis.com/css2?family=Segoe+UI:wght@400;700&display=swap"
+
+rel="stylesheet"
+
+/>
+</Head> <body> <Main /> <NextScript /> </body> </Html>
+
+);
+
+}
+
 }
 
 export default MyDocument;
--- pages/index.js
+++ pages/index.js
@@ -132,4 +132,4 @@ export default function Home() {
       </main>
     </div>
   );
-}
+} 
